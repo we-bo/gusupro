3cfc566a0a8cb2fe4a5c29042d006e60aaf5bddf
we_bo <2636477115@qq.com>
2020-10-16 22:52:55 +0800

    第二次提交：首页页面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    
+  <div>
+    <div class="div_first">
+    <h3>发现</h3>
+    <router-link to="javascript:;">全部城市</router-link>
+    <img src="../assets/gs_icon6.png" alt="">
+    </div>
+    <div>
+      <mt-swipe 
+      :showIndicators="true"
+      :auto="2000"
+      :spend="2000"
+      :continuous="true"
+      >
+        <mt-swipe-item></mt-swipe-item>
+      </mt-swipe>
+    </div>
   </div>
 </template>
+<style>
+.div_first{
+  display: flex;
+}
+.div_first>a{
+  color: rgba(50, 55, 70, 0.8);
+  margin-top: 7px;
+  font-size: 14px;
+  padding-left: 4px;
+}
+.div_first>h3{
+  font-size: 24px;
+}
+.div_first>img{
+  width: 2%;
+  height: 4%;
+  margin-top: 15px;
+  margin-left: 2px;
+}
+</style>
 
 <script>
 
 export default {
-  
+  data(){
+    return{
+      pics:[],
+    }
+  },
+  methods:{
+
+  },
+  mounted(){
+    this.axios.get('/category').then(result=>{
+      this.category = result.data.results;
+    })
+  }
 }
 </script>
